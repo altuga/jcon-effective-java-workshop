d39690deebcadc3b56bf9fb5254cd2dfb915c73a
AltugAltintas <altuga@gmail.com>
2019-09-23 15:22:44 +0200

    question 22  is done

--- src/main/java/com/kodcu/question22/MusicTeam.java
+++ src/main/java/com/kodcu/question22/MusicTeam.java
@@ -2,7 +2,17 @@ package com.kodcu.question22;
 
 //1
 public enum MusicTeam {
-    SOLO, DUET, TRIO, QUARTET, QUINTET,
-    SEXTET, SEPTET, OCTET, NONET, DECTET;
-    public int numberOfMusicians() { return ordinal() + 1; }
+    SOLO(1), DUET(2), TRIO(3), QUARTET(4), QUINTET(5),
+    SEXTET(6), SEPTET(7), OCTET(8), DOUBLE_QUARTET(8),
+    NONET(9), DECTET(10), TRIPLE_QUARTET(12);
+
+    private final int numberOfMusicians;
+
+    MusicTeam(int size) {
+        this.numberOfMusicians = size;
+    }
+
+    public int numberOfMusicians() {
+        return numberOfMusicians;
+    }
 }
