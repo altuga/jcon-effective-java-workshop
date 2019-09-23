6b82cdfa0bfd9ad0e702acf2c5051a4621e2b1ec
AltugAltintas <altuga@gmail.com>
2019-09-23 14:49:57 +0200

    question 19 is done

--- src/main/java/com/kodcu/question19/Combiner.java
+++ src/main/java/com/kodcu/question19/Combiner.java
@@ -13,17 +13,17 @@ TODO
 public class Combiner {
 
     //1
-    public static Set combine(Set s1, Set s2) {
-        Set result = new HashSet(s1);
-        result.addAll(s2);
-        return result;
-    }
+     public static <E> Set<E> combine(Set<E> s1, Set<E> s2) {
+             Set<E> result = new HashSet<>(s1);
+             result.addAll(s2);
+             return result;
+       }
 
     // Simple program to exercise generic method
     public static void main(String[] args) {
         Set<String> guys = Set.of("Tom", "Dick", "Harry");
         Set<Integer> ages = Set.of(44, 34, 21);
-        Set<String> aflCio = combine(guys, ages);
+        Set<String> aflCio = combine(guys, guys);
         System.out.println(aflCio);
     }
 }
