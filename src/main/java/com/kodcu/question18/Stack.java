f7ed6b6fd5990ad69414e09910f38705287e9760
AltugAltintas <altuga@gmail.com>
2019-09-23 14:23:40 +0200

    question 20 - solution 1

--- src/main/java/com/kodcu/question18/Stack.java
+++ src/main/java/com/kodcu/question18/Stack.java
@@ -9,31 +9,31 @@ TODO
  2 - What is the lesson  ?
 */
 
-public class Stack { // 1_1
-    public Object[] elements; // 1_2  and 2_1
+public class Stack<E> { // 1_1
+    private E[] elements;; // 1_2  and 2_1
     private int size = 0;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
 
 
     //1_3 and 2_2
     public Stack() {
-        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+         elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
     }
 
 
-    public void push(Object e) { //1_4
+    public void push(E e) { //1_4
         ensureCapacity();
         elements[size++] = e;
     }
 
 
-    public Object pop() { //1_5
+     public E pop()  { //1_5
         if (size == 0) {
             throw new EmptyStackException();
         }
 
 
-        Object result = elements[--size]; //1_6 and and 2_3
+        E result = elements[--size];; //1_6 and and 2_3
         elements[size] = null;
         return result;
     }
@@ -50,8 +50,8 @@ public class Stack { // 1_1
 
 
     public static void main(String[] args) {
-        Stack stack = new Stack();
-        stack.push("hello"); // write 12 and run again
+        Stack<String> stack = new Stack();
+        stack.push(12); // write 12 and run again
 
         String result = (String) stack.pop();
         System.out.println(result.toUpperCase());
